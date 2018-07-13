c7c37da0a4437bee5623a7e35030213ebd946571
Christoph Tack <prog.send@gmail.com>
2018-07-13 22:11:40 +0200

    Adding example with characther morphing of default Adafruit GFX library.
    Signed-off-by: Christoph Tack <prog.send@gmail.com>

--- examples/MorphSprite/MorphSprite.ino
+++ examples/MorphSprite/MorphSprite.ino
@@ -1,11 +1,14 @@
 #include "arduino8x8ledmatrix.h"
 #include "sprites.h"
 #include "morphbitmap.h"
+#include "glcdfont.c"
 
 const int pwmPin=6; //doesn't work with pin 9 on STM32 Nucleo
 Arduino8x8LedMatrix matrix(2,2,10, pwmPin, false);
 MorphBitmap bmp(256);
 byte morphBmp[8];
+byte charA[8];
+byte charB[8];
 
 void setup()
 {
@@ -17,6 +20,9 @@ void setup()
     Serial.println("started");
     matrix.begin();
     bmp.startMorph(test[0],test[1],8,8);
+    matrix.fillScreen(matrix.BLACK);
+    char2bitMap('1',morphBmp);
+    matrix.drawBitmap(0,0, morphBmp, 8, 8, matrix.RED);
 }
 
 void showMorph()
@@ -24,13 +30,14 @@ void showMorph()
     static unsigned long ulTimer=0;
     static byte state=0;
     static byte item=0;
+    const byte maxSteps=8;
     if(millis()>ulTimer+150)
     {
         ulTimer=millis();
-        bmp.getNextStep(morphBmp, state, 4);
+        bmp.getNextStep(morphBmp, state, maxSteps);
         matrix.fillScreen(matrix.BLACK);
         matrix.drawBitmap(0,0, morphBmp, 8, 8, matrix.RED);
-        state=(state==9?0:state+1);
+        state=(state==maxSteps+1?0:state+1);
         if(!state)
         {
             item=1-item;
@@ -52,10 +59,43 @@ void showPacman()
     }
 }
 
+void char2bitMap(char c, byte* bitmap)
+{
+    for(byte col=0;col<5;col++)
+    {
+        for(byte row=0;row<8;row++)
+        {
+            bitWrite(bitmap[row], 7-col, bitRead(font[c*5+col],row));
+        }
+    }
+}
+
+void showMorphChars()
+{
+    static unsigned long ulTimer=0;
+    static byte state=0;
+    static char c='0';
+    const byte maxSteps=8;
+    if(millis()>ulTimer+100)
+    {
+        if(!state)
+        {
+            delay(300);
+            char2bitMap(c++,charA);
+            char2bitMap(c,charB);
+            bmp.startMorph(charA, charB, 8, 8);
+        }
+        ulTimer=millis();
+        bmp.getNextStep(morphBmp, state, maxSteps);
+        matrix.fillScreen(matrix.BLACK);
+        matrix.drawBitmap(0,0, morphBmp, 8, 8, matrix.RED);
+        state=(state==maxSteps+1?0:state+1);
+    }
+}
 
 void loop()
 {
-    showMorph();
+    showMorphChars();
 }
 
 
